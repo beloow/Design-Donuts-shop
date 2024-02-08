3d5c3f17ad957ad1ca4b74af16b57ded4cc3b9c8
beloow <quevaqueva11@gmail.com>
2024-02-08 13:50:52 +0100

    Animation Update
    + Ajour d'animations au scroll
    + Test d'un player audio sur le site pour difuser une ambiance
    + Boost quantique

--- js/script.js
+++ js/script.js
@@ -15,3 +15,46 @@ menuIcon.addEventListener('click', () => {
         nav.style.transition = 'transform 0.2s ease-out';
         }
     });
+
+/*======================================================
+                        Audio player
+  ======================================================*/
+
+const button = document.querySelector('.play-button');
+const audio = document.querySelector('.audio');
+const icon = document.querySelector('.button-icons');
+
+button.addEventListener('click', () =>{
+    audio.volume = 0.02;
+    player();
+})
+
+function player(){
+    if (audio.paused) {
+        audio.play();
+        button.classList.toggle("play");
+        icon.classList.replace("fa-volume-xmark", "fa-volume-high");
+     } else {
+        audio.pause();
+        button.classList.toggle("play");
+        icon.classList.replace("fa-volume-high","fa-volume-xmark");
+     }
+}
+
+
+
+/*======================================================
+                        Scroll Reveal
+  ======================================================*/
+
+
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.donut-one, .donut-two, .donut-three, .donut-four, .cane-image, .img-box, .contact-section-container, .footer-content', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.about-content, .heading', { origin: 'right' });
